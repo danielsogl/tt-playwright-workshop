47349d439a33e20a004c3825af88bd567849cf65
Daniel Sogl <me@danielsogl.de>
2024-07-08 14:16:54 +0200

    add playwright test example

---
+++ playwright-tests/todo-mvc.spec.ts
@@ -0,0 +1,32 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('TodoMVC', () => {
+  test('should navigate to the TodoMVC app', async ({ page }) => {
+    await page.goto('');
+    await expect(page).toHaveTitle('TodoMVC: Angular');
+  });
+
+  test('should add a new todo', async ({ page }) => {
+    await page.goto('');
+    await page.getByRole('textbox', { name: 'What needs to be done?' }).fill('New Todo');
+    await page.getByRole('textbox', { name: 'What needs to be done?' }).press('Enter');
+    await expect(page.locator('app-todo-item').getByText('New Todo')).toBeVisible();
+  });
+
+  test('should complete a todo', async ({ page }) => {
+    await page.goto('');
+    await page.getByRole('textbox', { name: 'What needs to be done?' }).fill('New Todo');
+    await page.getByRole('textbox', { name: 'What needs to be done?' }).press('Enter');
+    await page.locator('app-todo-item').getByRole('checkbox').check();
+    await expect(page.locator('app-todo-item').getByRole('checkbox')).toBeChecked();
+  });
+
+  test('should delete a todo', async ({ page }) => {
+    await page.goto('');
+    await page.getByRole('textbox', { name: 'What needs to be done?' }).fill('New Todo');
+    await page.getByRole('textbox', { name: 'What needs to be done?' }).press('Enter');
+    await page.locator('app-todo-item').hover();
+    await page.locator('app-todo-item').getByRole('button', { name: '×' }).click();
+    await expect(page.locator('app-todo-item')).not.toBeVisible();
+  });
+});
