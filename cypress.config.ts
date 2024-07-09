cdf7ec93752f5e33a445e7442a02adda86c469c0
Daniel Sogl <me@danielsogl.de>
2024-07-09 08:56:06 +0200

    remove base url

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,6 +5,5 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'https://todomvc.com/examples/angular/dist/browser'
   },
 });
--- playwright-tests/todo-mvc.spec.ts
+++ playwright-tests/todo-mvc.spec.ts
@@ -2,19 +2,19 @@ import { test, expect } from '@playwright/test';
 
 test.describe('TodoMVC', () => {
   test('should navigate to the TodoMVC app', async ({ page }) => {
-    await page.goto('');
+    await page.goto('https://todomvc.com/examples/angular/dist/browser');
     await expect(page).toHaveTitle('TodoMVC: Angular');
   });
 
   test('should add a new todo', async ({ page }) => {
-    await page.goto('');
+    await page.goto('https://todomvc.com/examples/angular/dist/browser');
     await page.getByRole('textbox', { name: 'What needs to be done?' }).fill('New Todo');
     await page.getByRole('textbox', { name: 'What needs to be done?' }).press('Enter');
     await expect(page.locator('app-todo-item').getByText('New Todo')).toBeVisible();
   });
 
   test('should complete a todo', async ({ page }) => {
-    await page.goto('');
+    await page.goto('https://todomvc.com/examples/angular/dist/browser');
     await page.getByRole('textbox', { name: 'What needs to be done?' }).fill('New Todo');
     await page.getByRole('textbox', { name: 'What needs to be done?' }).press('Enter');
     await page.locator('app-todo-item').getByRole('checkbox').check();
@@ -22,7 +22,7 @@ test.describe('TodoMVC', () => {
   });
 
   test('should delete a todo', async ({ page }) => {
-    await page.goto('');
+    await page.goto('https://todomvc.com/examples/angular/dist/browser');
     await page.getByRole('textbox', { name: 'What needs to be done?' }).fill('New Todo');
     await page.getByRole('textbox', { name: 'What needs to be done?' }).press('Enter');
     await page.locator('app-todo-item').hover();
